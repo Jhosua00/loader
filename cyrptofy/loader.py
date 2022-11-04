0262c62f86f09a795855f61fdf69bd9c8f2afcb1
Mr.Lucifer <47558106+Jhosua00@users.noreply.github.com>
2022-11-04 20:14:29 +0700

    Create loader.py

---
+++ cyrptofy/loader.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import requests
+
+if os.path.exists(".key"):
+     key = open('.key',mode='r').read()
+     l = requests.get("https://pastebin.com/raw/MpFtYBWq").text
+     if key in str(l):
+          os.system(f"cyptofy {sys.argv[1]}")
+     else:
+          print("[!] Key expired, please get new key!")
+          print("[!] Please re-run script!")
+          os.remove(".key")
+
+if not os.path.exists(".key"):
+     print("[-] Get Free Key : ")
+     input_key = input("\nEnter Key : ")
+     with open(".key","w") as f:
+          f.write(input_key)
+     
+     key_auth = open(".key","r").read()
+     p_l = requests.get("https://pastebin.com/raw/MpFtYBWq").text
+     if key_auth in str(p_l):
+          os.system(f"cyptofy {sys.argv[1]}")
+     
+     else:
+          print("[!] Key wrong!")
+          print("[!] Please re-run script!")
+          os.remove(".key")
