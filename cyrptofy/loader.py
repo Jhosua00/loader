dcd1f6c609249248d59c3d9ef52b63b2db8c1fdd
Mr.Lucifer <47558106+Jhosua00@users.noreply.github.com>
2022-11-04 20:46:35 +0700

    Update loader.py

--- cyrptofy/loader.py
+++ cyrptofy/loader.py
@@ -2,9 +2,6 @@ import os
 import sys
 import requests
 
-GREEN = "\033[1;32m"
-RED = "\033[1;91m"
-WHITE = "\033[1;97m"
 
 if os.path.exists(".key"):
      key = open('.key',mode='r').read()
@@ -12,13 +9,13 @@ if os.path.exists(".key"):
      if key in str(l):
           pass
      else:
-          print(f"{WHITE}[{RED}!{WHITE}] Key expired, please get new key!")
-          print(f"{WHITE}[{RED}!{WHITE}] Please re-run script!")
+          print(f"[!] Key expired, please get new key!")
+          print(f"[!] Please re-run script!")
           os.remove(".key")
           sys.exit()
 
 if not os.path.exists(".key"):
-     print(f"[-] Get Free Key : {GREEN}https://safelink.id/K1IxUaqN")
+     print(f"[-] Get Free Key : https://safelink.id/K1IxUaqN")
      input_key = input(f"\n[-] Enter Key : ")
      with open(".key","w") as f:
           f.write(input_key)
